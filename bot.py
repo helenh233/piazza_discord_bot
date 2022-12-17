83a0f634039e2dccdd0c292b879a82a048e9b8f1
Helen <67713010+helenh233@users.noreply.github.com>
2022-12-16 22:08:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,68 +1,70 @@
-import discord # pip install -U discord.py
+import discord # (pip install -U discord.py)
 from discord.ext import tasks, commands # extension library
 
-from piazza_api import Piazza # pip install piazza-api
+from piazza_api import Piazza # (pip install piazza-api)
 from piazza_api import network
 
 from credentials import EMAIL, PASSWORD, DISCORD_TOKEN, IDs # Needs to be filled out
 from prettify import *
 
-#################################################
+###################################################################################
 
-# Creates an instance of the Piazza class, and logs in
+# Creates an instance of the class Piazza, and logs in
 p = Piazza()
 p.user_login(email=EMAIL, password=PASSWORD)
 
-# Connects to Discord bot, test out intent #
+# Connects to Discord bot,                                                        test out intent #
 client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == 'School':
-            break
-    print(f'{client.user} is connected to {guild.name}')
-    
+    print(f'{client.user} is connected to {guild.name}') # to console
     notifications.start(IDs)
 
-# Function will run every minute
+# Bot will scan for messages every minute
 @tasks.loop(minutes = 1)
 
-# notifications() will scan each class's Piazza feed and send updates to specific channels in a Discord server
-# List of (Str, Str, Int) -> None
-# Ex) notifications( [('Class name', 'Course's network ID on Piazza', 'Channel's ID on Discord')] )
+# notifications will scan each Piazza course in (ids) and send unread notifications to their corresponding Discord channel
+# List of (Str, Int) -> None
 async def notifications(ids):
     for ID in ids:
-        network_id = ID[1]
-        channel_id = ID[2]
+        network_id = ID[0]
+        channel_id = ID[1]
         
-        # Instance of Network class, for the Piazza course with (network_id)
+        # Instance of Network class, for the Piazza course corresponding to (network_id)
         course = p.network(network_id) 
 
-        # Instance of Channel , for Discord channel with (channel_id)
+        # Instance of Channel class, for Discord channel corresponding to (channel_id)
         channel = client.get_channel(channel_id)
 
         # List of unread Piazza posts
         unread = course.get_filtered_feed(network.UnreadFilter())['feed']
-        unread.reverse() # FIFO
+        # Put in chronological order
+        unread.reverse()
 
         for post in unread:
-            # Dictionary
+            '''
+            network.get_post(cid) returns all the information stored about the post corresponding to (cid)
+            Str -> Dict
+            '''
             info = course.get_post(cid=post['id'])
             
+            # Link to the Piazza post
             url = f"https://piazza.com/class/{network_id}/post/{info['nr']}"
             
             # Most recent action on that post
             action = info['change_log'][-1]['type']
             
+            # A question is created/updated:
             if action == 'create' or action == 'update':
-                # **bold** and __underline__ in Discord
-                subject = f"__**{info['history'][0]['subject']}**__" 
+                # Print post's subject as **bold** and __underline__ in Discord
+                subject = f"__**{info['history'][0]['subject']}**__"
+                # Translate HTML entities to Unicode characters
                 subject = BeautifulSoup(subject, 'html.parser').text
                 
                 content = info['history'][0]['content']
                 content = prettify(content)
-
+                # Send summary of the post as a message to the Discord channel
                 await channel.send(subject + '\n\n' + content + '\n\n' + url)
 
             elif info['is_bookmarked']:
