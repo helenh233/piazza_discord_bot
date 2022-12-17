a7e412c5f5cfd9288a4cb8ac4e6f3d8644e7e21f
Helen <67713010+helenh233@users.noreply.github.com>
2022-12-16 19:14:42 -0500

    Update prettify.py

--- prettify.py
+++ prettify.py
@@ -1,52 +1,71 @@
-import re # Regular Expression module
-import urllib.parse
-from bs4 import BeautifulSoup
+import re # Module that searches for Regular Expressions (built-in)
+import urllib.parse # Module that quotes URLs ()
+from bs4 import BeautifulSoup # (pip install beautifulsoup4)
 
-#################################################
+###############################################################################################
 
-# prettify changes all Piazza markdown conventions in (content) to Discord.
-# replaces images and latex with links to their source
-# Str -> Str
+'''
+prettify replaces all Piazza markdowns in (content) with their corresponding Discord markdowns
+Also replaces images with links, and LaTex with a link to the rendered image
+Str -> Str
+'''
 def prettify(content):
 
-    # Remove formatting on empty line breaks
+    # Removes formatting on empty line breaks ##################################################
     content = content.replace('<strong></strong>', '')
     content = content.replace('<em></em>', '')
-
-    # re.sub finds all instances of (pattern) in (string) and replaces it with (repl)
-    # Str Str Str -> Str
-    # Regular expressions used:
-        # . matches any character (except '\n')
-        # +? minimal quantifier (matches as few characters as possible)
-        #(causes the resulting Regular Expression to match 1+ repetitions of the preceding Regular Expression)
-
-    # Piazza -> Discord markdown
-    # <strong> bold </strong>   ->  ** bold **
+    content = content.replace('<code></code>', '')
+    content = content.replace('<span style="text-decoration:underline"></span>', '')
+    content = content.replace('<span style="text-decoration:line-through"></span>', '')
+    
+    # Piazza -> Discord markdown ##############################################################
+    '''
+    re.sub returns (string) with all instances of (pattern) replaced with (repl)
+    Str Str Str -> Str
+    Regular expressions used in the raw string (pattern):
+        '.+' matches a non-zero substring containing any characters (except '\n')
+        '?' matches as few characters as possible
+    Regular expressions used in the raw string (repl):
+        '\1' will be replaced with the matched substring enclosed by '()' in (pattern)
+    '''    
+    # <strong> bold </strong>   ->   ** bold **
     content = re.sub(pattern=r'<strong>(.+?)</strong>', repl=r'**\1**', string=content)
-    # <em> italics </em>    ->  * italics *
+    # <em> italics </em>   ->   * italics *
     content = re.sub(pattern=r'<em>(.+?)</em>', repl=r'*\1*', string=content)
-    # <code> code </code>   ->  ``` code ```
+    # <code> code </code>   ->   ``` code ```
     content = re.sub(pattern=r'<code>(.+?)</code>', repl=r'```\1```', string=content)
-    # <span style="text-decoration:underline"> underline </span>    ->  __ underline __
-    content = re.sub(pattern=r'<span style="text-decoration:underline">(.+?)</span>', repl=r'__\1__', string=content)
-    # <span style="text-decoration:line-through"> strikethrough </span>     ->  ~~ strikethrough ~~
-    content = re.sub(pattern=r'<span style="text-decoration:line-through">(.+?)</span>', repl=r'~~\1~~', string=content)
+    # <span style="text-decoration:underline"> underline </span>   ->   __ underline __
+    content = re.sub(pattern=r'<span style="text-decoration:underline">(.+?)</span>', 
+                     repl=r'__\1__', string=content)
+    # <span style="text-decoration:line-through"> strikethrough </span>   ->   ~~ strikethrough ~~
+    content = re.sub(pattern=r'<span style="text-decoration:line-through">(.+?)</span>', 
+                     repl=r'~~\1~~', string=content)
 
-    # Replace HTML image tags: <img src="____" /> with URL: https://piazza.com____
+    # Replaces HTML image tags with URL ###########################################################
+    # <img src="{source}" />   ->   https://piazza.com{source}
     content = re.sub(r'<img src="(.+?)".+?/>', repl=' https://piazza.com'+r'\1 ', string=content)
 
-    # findall returns a list of all occurrences that match a given (pattern) in (string)
-    # Str Str -> List of Str
+    # Replaces LaTex with a link to the rendered image ############################################
+    '''
+    findall returns a list of all occurrences of (pattern) in (string)
+    Str Str -> List of Str
+    '''
     latex = re.findall(pattern='\$\$.+?\$\$', string=content)
     for line in latex:
-        # Remove '$$' from beginning and end
-        # Str -> Str
+        '''
+        string.strip(chars) returns (string) with the leading and trailing (chars) removed
+        Str -> Str
+        '''
         tex = line.strip('$')
-        # Encode URLs
-        tex = urllib.parse.quote(tex)
-        # Replace with URL to image of compiled LaTex
-        content = content.replace(line, 'https://latex.codecogs.com/png.image?%5Cdpi%7B300%7D%5Cbg%7Bblack%7D'+tex)
+        '''
+        urllib.parse.quote(string) replaces special characters in (string) using the %xx escape
+        Used to encode the path section of a URL
+        Str -> Str
+        '''
+        path = urllib.parse.quote(tex)
+        content = content.replace(line, 'https://latex.codecogs.com/png.image?%5Cdpi%7B300%7D%5Cbg%7Bblack%7D'+path)
 
-    # >>> Discord quote
+    # >>> Discord quote markdown
     content = '>>> ' + BeautifulSoup(content, 'html.parser').text
+    
     return content
